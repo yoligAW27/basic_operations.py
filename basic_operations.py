c6c0e6bb131bf1be06a4f690cd476637f1e9d982
yoligAW27 <yohanagezahagn@gmail.com>
2025-11-13 14:36:44 -0800

    basic_operations.py

--- basic_operations.py
+++ basic_operations.py
@@ -3,4 +3,5 @@ number2 = 5
 
 print(f"Addition of {number1} and {number2} is {number1 + number2}")
 print(f"Subtraction of {number1} and {number2} is {number1 - number2}")
+
 print(f"Multiplication of {number1} and {number2} is {number1 * number2}")
